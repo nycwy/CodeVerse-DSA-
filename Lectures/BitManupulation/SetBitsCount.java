4a7b1615282f7a86d31bad85a09f2737c6561db2
nycwy <stuxy.1129@gmail.com>
2025-06-05 12:03:48 +0545

    Number of set bits in a number

---
+++ Lectures/BitManupulation/SetBitsCount.java
@@ -0,0 +1,15 @@
+public class SetBitsCount {
+    public static int countSets(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSets(18));
+    }
+}
